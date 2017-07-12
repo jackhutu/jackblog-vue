1c6dead1fc5d742c5ffaa561aedf55d3b30b7bea
Jack Hu <hutufeia2009@sina.com>
2017-07-12 20:43:28 +0800

    fix settings

--- src/components/Settings/index.vue
+++ src/components/Settings/index.vue
@@ -2,16 +2,20 @@
   <div class="settings-box">
     <div class="settings-container">
       <h2 class="title">设置</h2>
-      <hr></hr>
+      <hr />
       <div class="profile">
         <!-- 呢称邮件-->
         <div class="control-group" id="settingsForm">
           <form class="settings-form" @submit.prevent="mdUser()" novalidate>
             <div class="form-group">
-              <label class="control-label">昵称</label>
-              <input placeholder="2-15字符，中英文、数字和下划线" type="text" v-model="newNickname" v-validate="'required|nickname|min:4|max:30'" class="form-control">
+              <div class="form-group">
+                <label class="control-label">昵称</label>
+                <input type="text" name="nickname" v-model="newNickname" v-validate="'required|nickname|min:4|max:30'" class="form-control" placeholder="2-15字符，中英文、数字和下划线"></input>
+              </div>
             </div>
-            <button type="submit" class="btn btn-block btn-lg btn-primary" :disabled="!fields.valid()">保 存</button>
+            <div class="form-group">
+              <button type="submit" class="btn btn-block btn-lg btn-primary">保 存</button>
+            </div>
           </form>
         </div>
       </div>
@@ -23,7 +27,6 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 import { Validator } from 'vee-validate'
-
 export default {
   data() {
     return {
@@ -61,4 +64,3 @@ export default {
   }
 }
 </script>
-
